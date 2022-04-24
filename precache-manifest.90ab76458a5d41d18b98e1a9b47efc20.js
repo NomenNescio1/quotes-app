76a42503f6ccd8e89395df0cb8098f1b3b9f1c61
NomenNescio1 <u1201416@unimilitar.edu.co>
2022-04-23 21:33:15 -0500

    Updates

---
+++ precache-manifest.90ab76458a5d41d18b98e1a9b47efc20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebed36ebe87cbf86d87e29bb81ad6eb1",
+    "url": "/quotes-release/index.html"
+  },
+  {
+    "revision": "fae8c021587d42f9355b",
+    "url": "/quotes-release/static/css/main.a8667e55.chunk.css"
+  },
+  {
+    "revision": "99697b4b827a18de0cbd",
+    "url": "/quotes-release/static/js/2.160585df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quotes-release/static/js/2.160585df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fae8c021587d42f9355b",
+    "url": "/quotes-release/static/js/main.a164a6f8.chunk.js"
+  },
+  {
+    "revision": "93419ae96271cbd51fdb",
+    "url": "/quotes-release/static/js/runtime-main.f57bed6f.js"
+  }
+]);

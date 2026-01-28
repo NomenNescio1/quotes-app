78fde1f1c5b0dce1906347d6563de77ed0a5a0d5
NomenNescio1 <NomenNescio1@users.noreply.github.com>
2026-01-28 02:38:04 +0000

    deploy: 246d30625bee90ebc1ce456a2d77e5c52f5f4864

--- _next/static/b3CreYuYlzwlg1ei0Jmzf/_buildManifest.js
+++ _next/static/b3CreYuYlzwlg1ei0Jmzf/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/quotes//_next/:path+",
-        "destination": "/quotes/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
